c18e861a0eb1dfcaa025b18c65e79547292ea8f1
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-11-09 06:50:51 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,8 @@
-
+<?php 
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $con = mysqli_connect('fdb1029.awardspace.net','4238170_buzzbirdph','Titanvpn/101017','carproject');
+    if(!$con)
+    {
+        echo 'please check your Database connection';
+    }
+?>
